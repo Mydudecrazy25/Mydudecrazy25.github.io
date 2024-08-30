f7530b22c7b3fb9a37e7896275307ffc6a8cb7ee
Mydudecrazy25 <tristanch200808@gmail.com>
2024-08-30 14:23:13 -0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World! My name is Tristan, I am 16 years old. My Dad's name is Steve, My Mom's name is Heather. Caden and JJ are my brothers. Kenzie, Mikey, and kersten are my sisters.");
+    }
+}
