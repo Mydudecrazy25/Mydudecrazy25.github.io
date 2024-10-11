96e99edd9c33611635e6863af02766155e739a0b
Mydudecrazy25 <tristanch200808@gmail.com>
2024-10-11 11:56:23 -0600

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,28 @@
+// Tristan Chapman
+// Creating class object 2.3pt1
+// Mr gross Software and Programming2024
+  
+ { 
+  
+    // Announces/declares the string.
+    
+    String name = "Horsemom"; 
+  
+    
+    public static void main(String[] args) 
+    { 
+        
+        
+        
+        GFG obj = new GFG(); 
+  
+        // This prints the object name out.
+        System.out.println(obj.name); 
+    } 
+}
+    
+
+
+
+
+
